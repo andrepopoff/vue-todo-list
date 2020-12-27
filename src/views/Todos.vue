ae2d9bb13a534f9c6b4767f351327b41176f2aa3
Andrei Popov <andrepopoff@gmail.com>
2020-12-27 20:48:59 +0300

    Show a loader

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -4,27 +4,34 @@
         <router-link to="/">Home</router-link>
         <AddTodo @add-todo="addTodo"/>
         <hr>
-        <TodoList :todos="todos" @remove-todo="removeTodo"/>
+        <Loader v-if="loading" />
+        <TodoList v-else-if="todos.length" :todos="todos" @remove-todo="removeTodo"/>
+        <p v-else>No todos!</p>
     </div>
 </template>
 
 <script>
     import TodoList from "@/components/TodoList";
     import AddTodo from "@/components/AddTodo";
+    import Loader from "@/components/Loader";
     export default {
         name: 'App',
         data() {
             return {
-                todos: []
+                todos: [],
+                loading: true
             }
         },
         components: {
-            TodoList, AddTodo
+            TodoList, AddTodo, Loader
         },
         mounted() {
-            fetch('https://jsonplaceholder.typicode.com/todos')
+            fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
                 .then(response => response.json())
-                .then(json => this.todos = json)
+                .then(json => {
+                    this.todos = json;
+                    this.loading = false
+                })
         },
         methods: {
             removeTodo(id) {
