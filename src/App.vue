a75ef134b92d96efa979c2d80ed06956891fcb3d
Andrei Popov <andrepopoff@gmail.com>
2020-12-27 20:41:33 +0300

    Use vue routes

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
-    <AddTodo @add-todo="addTodo"/>
     <hr>
-    <TodoList :todos="todos" @remove-todo="removeTodo"/>
+    <router-view />
   </div>
 </template>
-
-<script>
-import TodoList from "./components/TodoList";
-import AddTodo from "./components/AddTodo";
-export default {
-  name: 'App',
-  data() {
-    return {
-      todos: []
-    }
-  },
-  components: {
-    TodoList, AddTodo
-  },
-  mounted() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => this.todos = json)
-  },
-  methods: {
-    removeTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodo(todo) {
-      this.todos.push(todo)
-    }
-  }
-}
-</script>
 
 <style>
 #app {
